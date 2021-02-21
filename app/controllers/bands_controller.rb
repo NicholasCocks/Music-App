48d180fe44c52fdb51903a040ff8620942f1a628
NicholasCocks <nickcocksofficial@gmail.com>
2021-02-20 20:11:35 -0500

    intialized bands model and views

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,34 @@
+class BandsController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def band_params
+
+    end
+end
